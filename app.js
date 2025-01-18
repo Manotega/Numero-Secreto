af14e3bc1e86bf12cb6ab97a79bf3a89f50ea2db
Lucas <lucas_manteiga@outlook.com>
2025-01-17 22:01:16 -0300

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10');
+let numeroMaximo = 100;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 0;
 
-if (chute == numeroSecreto) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`)
+    tentativas ++
+
+    if (chute == numeroSecreto) {
+        break;
+
+    } else if (chute < numeroSecreto) {
+        alert(`O numero secreto eh maior que ${chute}`);
+
+    } else if (chute > numeroSecreto) {
+        alert(`O numero secreto eh menor que ${chute}`);
+    }
 }
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} em ${tentativas} ${palavraTentativa}!`);
+// if (tentativas == 1) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} em ${tentativas} tentativa!`)
+// } else {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} em ${tentativas} tentativas!`)
+// }
